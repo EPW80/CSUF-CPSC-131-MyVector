4cc82484a0ebae4933037c98c59beeff2b2221f3
EWP80 <erikpw009@gmail.com>
2023-02-20 22:00:46 -0800

    debugging

--- main.cpp
+++ main.cpp
@@ -1,19 +1,10 @@
 
-/**
- * You don't have to modify this source file, but can if you want.
- * This will not be used for grading, so you can use it to debug your
- * vector class.
- * This is the program that runs with "make run"
- */
 
-///	Your welcome
 #include <iostream>
 #include "MyVector.hpp"
 
-///	Your welcome
 using std::cout, std::endl;
 
-//
 int main()
 {
 	//
