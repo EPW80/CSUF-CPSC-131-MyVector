374bacaf1726953a9edf9b27d4b7775355dfba14
EWP80 <erikpw009@gmail.com>
2023-02-20 23:10:13 -0800

    going to bed im tired

--- main.cpp
+++ main.cpp
@@ -7,8 +7,17 @@ using std::cout, std::endl;
 
 int main()
 {
-	//
-	std::cout << "Hello! Maybe use this source file for debugging?" << std::endl;
+
+	MyVector<int> vec;
+
+	vec.push_back(1);
+	vec.push_back(2);
+	vec.push_back(3);
+
+	for (size_t i = 0; i < vec.size(); i++)
+	{
+		std::cout << vec[i] << " ";
+	}
 
 	return 0;
 }
